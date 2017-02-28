4df0864f72e5d1c04501012ce857e7c7cda4e9de
Hao Wu <haowu@Haos-MacBook-Pro.local>
2017-02-28 02:37:37 -0600

    iris pratice, and tensorflow saver

--- titanic/nn_tf.py
+++ titanic/nn_tf.py
@@ -7,6 +7,8 @@ import csv
 import random
 
 import tensorflow as tf
+import numpy as np
+import os
 
 FLAGS = None
 
@@ -67,13 +69,65 @@ def extract_label_and_feature(data):
     return labels, selected_features
 
 
-def main(_):
+def import_data():
     # Import data
     data = read_csv_data("./train.csv")
     random.shuffle(data)
     train_labels, train_features = extract_label_and_feature(data[:700])
     test_labels, test_features = extract_label_and_feature(data[700:])
+    return train_labels, train_features, test_labels, test_features
 
+
+def model(features, labels, mode):
+    # creating customized model
+    W1 = tf.Variable(tf.zeros([12, 12]))
+    b1 = tf.Variable(tf.zeros([12]))
+    z1 = tf.matmul(features['x'], W1) + b1
+    y1 = tf.sigmoid(z1)
+    W2 = tf.Variable(tf.zeros([12, 8]))
+    b2 = tf.Variable(tf.zeros([8]))
+    z2 = tf.matmul(y1, W2) + b2
+    y2 = tf.sigmoid(z2)
+    # W3 = tf.Variable(tf.zeros([8, 4]))
+    # b3 = tf.Variable(tf.zeros([4]))
+    # z3 = tf.matmul(y2, W3) + b3
+    # y3 = tf.sigmoid(z3)
+    W4 = tf.Variable(tf.zeros([8, 2]))
+    b4 = tf.Variable(tf.zeros([2]))
+    y = tf.matmul(y2, W4) + b4
+
+    # Define loss and optimizer
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=labels))
+    train_step = tf.train.GradientDescentOptimizer(
+        0.2).minimize(cross_entropy)
+    train = tf.group(train_step)
+    return tf.contrib.learn.ModelFnOps(
+        mode=mode, predictions=y,
+        loss=cross_entropy,
+        train_op=train)
+
+
+def main2(_):
+    # call contrib things
+    train_labels, train_features, test_labels, test_features = import_data()
+
+    estimator = tf.contrib.learn.Estimator(model_fn=model)
+    # define our data set
+    train_features_np = np.array(train_features, dtype=np.float32)
+    train_labels_np = np.array(train_labels, dtype=np.float32)
+    input_fn = tf.contrib.learn.io.numpy_input_fn(
+        {"x": train_features_np}, train_labels_np, 50, num_epochs=1000)
+
+    # train
+    estimator.fit(input_fn=input_fn, steps=1000)
+    # evaluate our model
+    print(estimator.evaluate(input_fn=input_fn, steps=10))
+
+
+def main(_):
+    # Import data
+    train_labels, train_features, test_labels, test_features = import_data()
     train_labels_batch = list()
     train_features_batch = list()
     batch_num = 10
@@ -89,7 +143,6 @@ def main(_):
                 train_labels[(i * batch_size):((i + 1) * batch_size)])
             train_features_batch.append(
                 train_features[(i * batch_size):((i + 1) * batch_size)])
-
 
     # Create the model
     x = tf.placeholder(tf.float32, [None, 12])
@@ -121,13 +174,22 @@ def main(_):
     # init Variables (trainable variables)
     tf.global_variables_initializer().run()
     # Train
-    for _ in range(10000):
+    for _ in range(1000):
         for i in range(0, len(train_features_batch)):
             sess.run(
                 train_step,
                 feed_dict={
                     x: train_features_batch[i],
                     y_: train_labels_batch[i]})
+
+    # save the trained model
+    saver = tf.train.Saver({
+        'W1': W1, 'b1': b1,
+        'W2': W2, 'b2': b2,
+        'W4': W4, 'b4': b4})
+    if not os.path.exists('./checkpoints'):
+        os.makedirs('./checkpoints')
+    saver.save(sess, './checkpoints/my-model')
 
     # Test trained model
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
